07c0eafae0937c2af1b4c68003b49f53bb436d8d
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-10-23 17:49:09 -0400

    add emoji for particular votes number

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,16 +1,68 @@
 import React, { Component } from 'react';
-
+import './Joke.css';
 class Joke extends Component {
+  getColor() {
+    if (this.props.votes >= 15) {
+      return '#4CAF50';
+    }
+    if (this.props.votes >= 12) {
+      return '#8BC34A';
+    }
+    if (this.props.votes >= 9) {
+      return '#CDDC39';
+    }
+    if (this.props.votes >= 6) {
+      return '#FFEB3B';
+    }
+    if (this.props.votes >= 3) {
+      return '#FFC107';
+    }
+    if (this.props.votes >= 0) {
+      return '#FF9800';
+    }
+    return '#f44336';
+  }
+
+  getEmoji() {
+    if (this.props.votes >= 15) {
+      return 'em em-rolling_on_the_floor_laughing';
+    }
+    if (this.props.votes >= 12) {
+      return 'em em-laughing';
+    }
+    if (this.props.votes >= 9) {
+      return 'em em-smiley';
+    }
+    if (this.props.votes >= 6) {
+      return 'em em-slightly_smiling_face';
+    }
+    if (this.props.votes >= 3) {
+      return 'em em-neutral_face';
+    }
+    if (this.props.votes >= 0) {
+      return 'em em-confused';
+    }
+    return 'em em-angry';
+  }
+
+
   render() {
     const { votes, text, upVote, downVote } = this.props;
     return (
       <div className='Joke'>
-        <div className='Joke-button'>
+        <div className='Joke-buttons'>
           <i className='fas fa-arrow-up' onClick={upVote} />
-          <span>{votes}</span>
+          <span className='Joke-votes' style={{ borderColor: this.getColor() }}>
+            {votes}
+          </span>
           <i className='fas fa-arrow-down' onClick={downVote} />
         </div>
-        <span>{text}</span>
+        <div className='Joke-text'>{text}</div>
+        <div className='Joke-smiley'>
+          <i
+            class={this.getEmoji()}
+            aria-label='ROLLING ON THE FLOOR LAUGHING'></i>
+        </div>
       </div>
     );
   }
